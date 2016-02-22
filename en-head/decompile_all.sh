d56859051be63a828247159464367a6cdeedcdac
vvasuki <vishvas.vasuki@gmail.com>
2016-02-22 00:24:01 -0800

    asdf

--- en-head/decompile_all.sh
+++
@@ -1,9 +0,0 @@
-STARDICT_TOOLS_DIR=~/stardict/tools/src/
-DECOMPILE=$STARDICT_TOOLS_DIR/stardict2txt
-
-for ifo_file in `ls */*.ifo`
-do
-	tsv_file=`ls $ifo_file|sed s/.ifo/.tsv/`
-	$DECOMPILE $ifo_file $tsv_file
-	# mv $txt_file $tsv_file
-done
